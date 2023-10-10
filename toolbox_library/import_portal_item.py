8ebd718403064a7c6a2823fc9a0cae793a5a79e6
David Aalbers <david.aalbers@gmail.com>
2023-10-10 15:49:59 +1300

    Add files via upload

--- toolbox_library/import_portal_item.py
+++ toolbox_library/import_portal_item.py
@@ -6,12 +6,14 @@ def log(message):
     arcpy.AddMessage(message)
 
 
-def do_execute(gis: arcgis.GIS, item_id: str, title: str = None):
-    log(f"Connected to {gis.url}")
+def do_execute(target_gis: arcgis.GIS, item_id: str, title: str = None):
+    home_gis = arcgis.GIS("https://arcgis.com")
+    log(f"Searching {home_gis.url}")
+    log(f"Cloning to {target_gis.url}")
 
     # source webmap (template)
     log(f"Searching for item by ID: {item_id}")
-    src_item = gis.content.get(item_id)
+    src_item = home_gis.content.get(item_id)
     src_item_props = {}
     for key in src_item:
         val = src_item[key]
@@ -22,7 +24,7 @@ def do_execute(gis: arcgis.GIS, item_id: str, title: str = None):
 
     # copy to new web map item
     log(f"Copying item as: {src_item_props['title']}")
-    target_item = gis.content.add(src_item_props)
+    target_item = target_gis.content.add(src_item_props)
     target_data = target_item.get_data()
     target_data.update(src_data)
     target_item.update(data=target_data)
